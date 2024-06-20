d4ffb747c8c9f118e5bae7d3177588b1a447ce42
Psychie42 <86179489+Psychodeath42@users.noreply.github.com>
2024-06-19 19:36:14 -0700

    Add files via upload

--- Tower_Of_Babel.js
+++ Tower_Of_Babel.js
@@ -1,3 +1,4 @@
+//reusing code, any good coder steals code, but i'm stealing from myself, so its not stealing, its just being smart.
 // Drop down menu code
 function displayMedia() {
     var selectedValue = document.getElementById("mediaDropdown").value;
@@ -14,4 +15,12 @@ function displayMedia() {
     } else if (selectedValue === "hampsters_with_cat") {
         displayArea.innerHTML = '<video onloadstart="this.volume=0.25" autoplay controls src="memes/hampster_cat.mp4"></video>';
     }
+}
+function play_sound() {
+        //gets area where sound controls will spawn
+        var sound_area = document.getElementById("sound_area");
+        // sets that area to the sound and autoplays it
+            sound_area.innerHTML = '<audio onloadstart="this.volume=0.25" autoplay controls loop src="memes/ambispace4.ogg"></audio> <br>';
+            // sets the text to black for readability
+            document.body.style.color = 'black';
 }
